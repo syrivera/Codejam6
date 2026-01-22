2b2d4547d2ca46cf634f42e79ba753ff8712f322
Michael Damiano <jmfrhta@jmfamily.com>
2026-01-22 15:20:50 -0500

    adding in primary key notation

--- codejam5b.client/api/Models/UserProgress.cs
+++ codejam5b.client/api/Models/UserProgress.cs
@@ -2,7 +2,10 @@ namespace Api.Models;
 
 public class UserProgress
 {
+    [Key]
+    [Column("progress_id")]
     public int progress_id { get; set; }
+
     public double current_weight { get; set; }
     public double target_weight { get; set; }
     public int consumed_calories { get; set; }
